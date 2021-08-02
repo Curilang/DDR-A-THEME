f717cd6bd62e0241a69042ae8dc6bc75fb9a99ec
Curilang <curilang30@gmail.com>
2021-08-02 02:49:48 -0500

    Updated Theme

--- BGAnimations/ModDate.lua
+++ BGAnimations/ModDate.lua
@@ -2,7 +2,7 @@ local t = Def.ActorFrame {
 Def.BitmapText{
       Font="_futura pt medium 30px";
 	  --                       MDX:A:A:A:2018/04/23/00
-      InitCommand=cmd(settext,"MDX:A:A:A:2021070100";halign,0;xy,SCREEN_LEFT+8.5,SCREEN_TOP+13;zoomx,0.62;zoomy,0.6;strokecolor,color("0,0,0,0.5"));
+      InitCommand=cmd(settext,"MDX:A:A:A:2021080100";halign,0;xy,SCREEN_LEFT+8.5,SCREEN_TOP+13;zoomx,0.62;zoomy,0.6;strokecolor,color("0,0,0,0.5"));
     };
 };
 
